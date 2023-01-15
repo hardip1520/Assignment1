4c05b13a7d78b922bb48ce05c1ff6bb168d6dc02
Hardipsinh Matroja <hardipsinhmatroja1520@gmail.com>
2023-01-15 12:10:19 -0500

    updated arrow

--- Pmodules/main.js
+++ Pmodules/main.js
@@ -2,6 +2,7 @@ import {halfOf} from './lib.js';
 import { mul } from './lib.js';
 import doSomething from './dosomething.js';
 import {flag, touch} from './validator.js';
+import {div} from './arrow.js';
 
 console.log(halfOf(25));
 console.log(mul(22,2))
@@ -9,3 +10,4 @@ doSomething();
 console.log(flag);
 touch()
 console.log(flag);
+console.log(div(50,2));
